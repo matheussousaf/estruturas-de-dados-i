e85a4fe904326e45ef498560091944100778feb6
Matheus de Sousa <matheussousaf7@gmail.com>
2019-09-13 12:41:18 -0300

    Subindo exercício

---
+++ ExercicioHistoricoEscolar/README.md
@@ -0,0 +1,49 @@
+## Exercício "Mini Trabalho" - LISTAS
+
+<div align="center"">
+    <p style="color: black;"><b>HISTÓRICO ESCOLAR</b></p>
+</div>
+
+* **Problema**: capturar do teclado, armazenar em memória e imprimir o Histórico Escolar de cada aluno de uma determinada turma. 
+
+* **Algoritmo base**: ler do teclado a “matrícula” de um aluno e, para cada matrícula, ler “nome da disciplina” e “nota final” deste aluno, armazenar em memória para posterior impressão de todos históricos. Controlar os laços de fim de disciplinas e fim de alunos perguntando se tem mais uma disciplina e se tem mais um aluno.
+
+* **O que utilizar**: lista sequencial para armazenar as triplas “disciplina – nota – endereço” de cada aluno e uma lista sequencial para armazenar a matrícula do aluno e o endereço do primeiro nó da lista de disciplinas desse aluno.
+
+* **Premissas**: Considere que a turma tem 60 alunos; considere que cada aluno tem uma quantidade fixa de três de disciplinas/notas/endereço cursadas até o momento. (esse endereço é um dado qualquer sem uso neste momento e pode conter qualquer valor ou NULL)
+
+	
+
+Instruções para a resolução e entrega:
+
+1 – O mini trabalho é individual. Serão desconsiderados os trabalhos considerados iguais.
+
+2 – O mini trabalho deve ser depositado na sala virtual da cadeira para avaliação e pontuação.
+
+3 – Data de entrega: até 17/09/2019 às 23:59hs.
+
+
+
+4 – Seu programa deve apresentar o seguinte menu
+
+
+```ALUNO: NOME COMPLETO DO ALUNO
+DISCIPLINA: ESTRUTURA DE DADOS I
+PROFESSOR: WALACE BONFIM
+
+HISTÓRICO ESCOLAR
+
+1 – INSERIR MATRÍCULAS, DISCIPLINAS E NOTAS 
+2 – EXIBIR HISTÓRICOS
+0 – SAIR
+
+DIGITE SUA OPÇÃO: 
+
+Opção 1: fazer a entrada de dados em dois laços aninhados: o externo para matrículas e o interno para disciplinas e notas; 
+Opção 2: exibir na tela os históricos separados por matrícula; 
+Opção 0: liberar toda memória alocada e encerrar o programa.
+
+```
+
+
+*Adaptado de EDELWEISS, Nina, LIVI, Maria Castro. Algoritmos e Programação com Exemplos em Pascal e C -Série Livros Didáticos UFRGS - Volume 23. Bookman, 01/2014. Pag. 434 [acervo digital].*
---
+++ ExercicioHistoricoEscolar/src/main.c
@@ -0,0 +1,162 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+#include <string.h>
+#include <unistd.h>
+
+//Tipos
+typedef struct Aluno Aluno;
+
+typedef struct Disciplina Disciplina;
+
+struct Disciplina{
+    char nome[30];
+    double nota;
+    Aluno *alunoReferente;
+};
+
+struct Aluno{
+    char matricula[15];
+    Disciplina disciplinas[3];
+};
+
+typedef struct lista_Aluno{
+    Aluno alunos[60];
+    int i;
+}lista_Aluno;
+
+
+//Funcionalidades
+lista_Aluno criar(); //Cria a lista
+Aluno cadastroAluno(Aluno *aluno); //Cadastra o aluno
+void cadastrarNovoAluno(lista_Aluno *listaDeAlunos); //Opção de cadastrar no menu
+void verHistorico(char matricula[], lista_Aluno *listaDeAlunos); //Opção de ver o histórico no menu
+
+//Funções auxiliares
+void clear();
+void printMenu();
+
+int main(){
+
+    //Mudando typeset pra Português
+    setlocale(LC_ALL, "Portuguese");
+
+    //Inicializando a lista de alunos
+    lista_Aluno listaDeAlunos = criar();
+
+    int esc = 4;
+    char matricula[15];
+
+    //Cabeçalho
+    printf("Aluno: Matheus de Sousa Figueirêdo\n");
+    printf("Matrícula: 1820023833\n");
+    printf("Ciências da Computação - Terceiro Período\n");
+    printf("\n");
+
+    //Loop principal
+    while(1){
+        if(esc == 0) break;
+        
+        //Printando o menu
+        printf("\n");
+        printMenu();
+        printf("\n");
+
+        printf("-> Escolha uma opção: ");
+        scanf("%d", &esc);
+        clear();
+
+        switch (esc){
+        case 0:
+            printf("# Você saiu do programa!\n");
+            break;
+        case 1:
+            cadastrarNovoAluno(&listaDeAlunos);
+            break;
+        case 2:
+            printf("# Digite a matrícula: ");
+            scanf("%s", matricula);
+            clear();
+            verHistorico(matricula, &listaDeAlunos);
+            break;
+        default:
+            printf("# Comando não existente!\n");
+        }
+    }
+    return 0;
+}
+
+void printMenu(){
+    printf("===== MENU DE OPÇÕES =====\n");
+    printf("1 - Cadastrar um aluno\n");
+    printf("2 - Exibir históricos (por matricula)\n");
+    printf("0 - Sair\n");
+    printf("==========================\n");
+}
+
+Aluno cadastroAluno(Aluno *aluno){
+    int i;
+
+    printf("Digite aqui a matrícula: ");
+    scanf("%s", aluno->matricula);
+    clear();
+
+    for (i = 0; i < 3; i++){
+        printf("Nome da %dª disciplina: ", i+1); 
+        scanf("%s" , aluno->disciplinas[i].nome);
+
+        printf("Nota: "); 
+        scanf("%lf", &aluno->disciplinas[i].nota);
+        clear();
+    }
+
+    return *aluno;
+}
+
+void cadastrarNovoAluno(lista_Aluno *listaDeAlunos){
+    char esc = 's';
+
+    //Preenchendo o listaAlunos
+    while(1){
+        if(esc == 'N' || esc == 'n') break;
+        listaDeAlunos->alunos[listaDeAlunos->i] = cadastroAluno(&listaDeAlunos->alunos[listaDeAlunos->i]);
+        listaDeAlunos->i++;
+
+        printf("Deseja continuar? [S/N]: "); 
+        scanf("%c", &esc);
+        clear();
+    }
+}
+
+void verHistorico(char *matricula, lista_Aluno *listaDeAlunos){
+    int i, j;
+
+    printf("# Pesquisando por matrícula '%s' em %d registros...\n", matricula, listaDeAlunos->i);
+    sleep(1);
+
+    for (i = 0; i < listaDeAlunos->i; i++){
+        if(strcmp(matricula, listaDeAlunos->alunos[i].matricula) == 0){
+            printf("# Matrícula encontrada!\n");
+
+            for (j = 0; j < 3; j++){
+                printf("%dª Disciplina: %s\n", j+1, listaDeAlunos->alunos[i].disciplinas[j].nome);
+                printf("Nota: %.2lf\n", listaDeAlunos->alunos[i].disciplinas[j].nota);
+            }
+            return;
+        }
+        else printf("Posição atual: %d\n", i);
+    }
+    printf("# Nenhum aluno encontrado na base de dados!\n");
+}
+
+lista_Aluno criar(){
+    lista_Aluno lista;
+    lista.i = 0;
+
+    return lista;
+}
+
+void clear(){
+    int c;
+    while((c = getchar()) != '\n' && c != EOF);
+}
