53a8f792c25f0ecc5a007ad6f4943dfd2663e1c5
Matheus de Sousa <matheussousaf7@gmail.com>
2019-10-28 22:32:00 -0300

    Adicionando exercício fila e pilha

---
+++ ExercicioPilhasEFilas/pilha.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+//criarPilha
+	
+//isVazia
+	
+//isCheia
+	
+//getUltimo
+	
+//push
+	
+//pop
+	
+//exibir
+
+int main(){
+	/*
+	# Exercício de Pilha, implementação do slide 19.
+	
+	Descrição: Implementar uma pilha sequencial.
+	
+	Funções a serem ser implementadas:
+	
+	- criarPilha();
+	- isVazia();
+	- isCheia();
+	- getUltimo();
+	- push();
+	- pop();
+	- exibir();
+	
+	*/
+
+	return 0;
+}
